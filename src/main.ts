a66f4fa7b4b2f04363ee271529129c6623f27d53
Lileeee <2682549139@qq.com>
2024-03-29 00:10:09 +0800

    更新：引入UI库antdv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
+import antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import App from '@/App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(antd).mount('#app')
